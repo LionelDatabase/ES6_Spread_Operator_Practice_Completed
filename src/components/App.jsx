b8de140464a6d9e80d64a9ebc46f0a698676bc07
LionelDatabase <73809432+LionelDatabase@users.noreply.github.com>
2020-12-10 00:59:37 +0800

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+function App() {
+  let k = 0;
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const value = event.target.value;
+    setInputText(value);
+  }
+
+  function addItem(event) {
+    // setItems([...items, inputText]);
+    setItems((prevItem) => {
+      return [...prevItem, inputText];
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input
+          type="text"
+          name="item"
+          onChange={handleChange}
+          value={inputText}
+        />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((todoItem) => {
+            return <li key={k++}>{todoItem}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
